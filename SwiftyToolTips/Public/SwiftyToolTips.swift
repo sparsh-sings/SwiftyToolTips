45b002fb4681656c1e02878b7171a9f466a6acce
Sparsh Singh <sparsh.singh@kalagato.co>
2024-03-25 12:46:03 +0530

    v.0.1

--- SwiftyToolTips/Public/SwiftyToolTips.swift
+++ SwiftyToolTips/Public/SwiftyToolTips.swift
@@ -5,17 +5,42 @@
 //  Created by Sparsh Singh on 21/03/24.
 //
 
+import UIKit
 
 public final class SwiftyToolTips : NSObject {
     
     public static let shared = SwiftyToolTips()
     
+    private let tooltipVC = ToolTipVC()
+    
 }
 
 extension SwiftyToolTips {
     
-    public func printSomething() -> String {
-        return "Third Party Library Working Successfully"
+    public func showToolTip(onItem viewItem: UIView, cmView: UIView, arrowDirection: UIPopoverArrowDirection = .any, viewSize: CGSize? = nil) {
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let topViewController = windowScene.windows.first?.rootViewController else {
+            fatalError("Unable to get the topmost view controller.")
+        }
+        
+        let size = CGSize(width: CGFloat(246), height: CGFloat(112))
+        tooltipVC.showToolTip(onItem: viewItem, cmView: cmView, arrowDirection: arrowDirection, viewSize: viewSize ?? size)
+        
+        var presentedViewController = topViewController
+        while let presented = presentedViewController.presentedViewController {
+            presentedViewController = presented
+        }
+        
+        presentedViewController.present(tooltipVC, animated: true)
+    }
+    
+    public func dismissToolTip(animated : Bool) {
+        tooltipVC.dismiss(animated: animated)
+    }
+    
+    public func nibView<T: UIView>(customView: T.Type, named: String? = nil) -> T {
+        return T.fromNib(named: named)
     }
     
 }
--- SwiftyToolTipsDemo/ViewController.swift
+++ SwiftyToolTipsDemo/ViewController.swift
@@ -9,14 +9,31 @@ import UIKit
 import SwiftyToolTips
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainButton: UIButton!
+    
+    let customeView = SwiftyToolTips.shared.nibView(customView: TempView.self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let object = SwiftyToolTips.shared.printSomething()
-        print(object)
+        
+        customeView.btnClose.addTarget(self, action: #selector(dismissPop), for: .touchUpInside)
+        customeView.lblTitle.text = "Lorem Ipsum is simply"
+        customeView.lblDescription.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        
+        
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        SwiftyToolTips.shared.showToolTip(onItem: mainButton, cmView: customeView, arrowDirection: .up)
+    }
+    
+    @objc func dismissPop() {
+        SwiftyToolTips.shared.dismissToolTip(animated: true)
+    }
 
 }
 
